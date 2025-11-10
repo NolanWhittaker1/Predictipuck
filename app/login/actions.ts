8d0f137d078827159bcf72c90218ce5b43306010
Nolan Whittaker <nolanwhittaker1@gmail.com>
2025-11-10 13:04:05 -0800

    First commit

---
+++ app/login/actions.ts
@@ -0,0 +1,5 @@
+"use server";
+
+export async function login(prevState: any , formData: FormData) {
+
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useActionState, useState } from "react";
+import { login } from "./actions";
+import { Input } from "@/components/ui/input"
+
+export default function Login() {
+
+    const [state, loginAction] = useActionState(login, undefined);
+    const [favteam, setFavTeam] = useState('ANA');
+
+    return (
+        <div>
+            <h1>Hello world</h1>
+            <form action={loginAction}>
+                <Input type="email" placeholder="Email" name="email"></Input>
+                <Input type="password" placeholder="Password" name="password"></Input>
+            </form>
+        </div>
+    );
+}
